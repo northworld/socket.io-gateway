96b95f04b822ba957c809691ebc98fc2c166ff35
Eric Reynolds <eric.reynolds@zoomeranalytics.com>
2016-11-28 11:37:41 +0000

    socket.io message labels

--- app/index.js
+++ app/index.js
@@ -1,13 +1,20 @@
 var server = require("http").createServer();
 var io = require("socket.io")(server);
 
+console.log("Connecting to Redis @ " + process.env.REDIS_REALTIME + "...")
 var redis = require("redis").createClient(process.env.REDIS_REALTIME);
 
 redis.subscribe("clients");
 
 redis.on("message", function(channel, message) {
-    console.log("REDIS " + message);
-    io.emit("redis", message);
+    try {
+        console.log("::" + message);
+        var info = JSON.parse(message);
+        io.emit(info.label, info.payload);
+    }
+    catch(e) {
+        console.log(e);
+    }
 });
 
 server.listen(8080, function() {
